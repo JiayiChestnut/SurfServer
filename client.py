692bdec2d73ce93d1b736c5fad7441310f2c6165
JiayiChestnut <chestnutljy@outlook.com>
2018-11-26 05:57:48 -0500

    first version

---
+++ client.py
@@ -0,0 +1,243 @@
+import rpyc
+import hashlib
+import os
+import sys
+#import timeit
+import time
+DEBUG = False
+"""
+A client is a program that interacts with SurfStore. It is used to create,
+modify, read, and delete files.  Your client will call the various file
+modification/creation/deletion RPC calls.  We will be testing your service with
+our own client, and your client with instrumented versions of our service.
+"""
+class ErrorResponse(Exception):
+	def __init__(self, message):
+		super(ErrorResponse, self).__init__(message)
+		self.error = message
+
+	def missing_blocks(self, hashlist):
+		self.error_type = 1
+		self.missing_blocks = hashlist
+
+	def wrong_version_error(self, version):
+		self.error_type = 2
+		self.current_version = version
+
+	def file_not_found(self):
+		self.error_type = 3
+
+def sha256(byteStr):
+    # m = hashlib.sha256(str.encode(s))
+	res = hashlib.sha256(byteStr).hexdigest()
+	return res
+
+def create_blocklist(filename):
+    try:
+        file = open(filename, 'rb')
+        hash_block_map = {}
+        while True:
+            block = file.read(4096)
+            read_amt = len(block)
+
+            if read_amt != 0:
+                _hash = sha256(block)
+                hash_block_map[_hash] = block
+            else:
+                break
+
+        return hash_block_map
+
+    except IOError: return None
+
+
+def findServerHash(h, numBlockStores):
+	return int(h,16) % numBlockStores
+
+
+
+class SurfStoreClient():
+    """
+    Initialize the client and set up connections to the block stores and
+    metadata store using the config file
+    """
+    def __init__(self, config):
+        with open(config, "r") as f:
+            lines = f.read().split("\n")
+        self.block_num = int(lines[0].split(":")[-1])
+
+
+
+
+        #set up conn with blocks
+        self.block_list = []
+        for i in range(2, 2 + self.block_num):
+            infos = lines[i].split(":")
+            self.block_list.append(rpyc.connect(infos[1].strip(), int(infos[2])).root)
+
+        #set up conn with meta store
+        infos = lines[1].split(":")
+#        print(infos)
+        self.metadataStore = rpyc.connect(infos[1].strip(), int(infos[2])).root
+        
+        #choose the server finding
+        self.findServerOption = int(lines[2 + self.block_num])
+        """
+        	upload(filepath) : Reads the local file, creates a set of
+
+
+        	hashed blocks and uploads them onto the MetadataStore
+        	(and potentially the BlockStore if they were not already present there).
+        	"""
+    def upload(self, filepath):
+        #create block list
+        hash_block_map = create_blocklist(filepath)
+        if (hash_block_map == None):
+            raise IOError("missing file : %s !" % (filepath,))
+        filename = os.path.basename(filepath)
+#        filename = filepath
+
+
+        #create new blocks if not exist
+        #always use blockStore 0 to store things
+        if (self.findServerOption == 1):
+            for h in hash_block_map.keys():
+                exist = False
+                for b in self.block_list:
+                    #check blocks exists, make sure the min_Block to insert
+                    if b.has_block(h):
+                        exist = True
+                        break
+                if not exist:
+                    #need to insert to block
+                    block2insert = self.block_list[findServerHash(h, self.block_num)]
+                    block2insert.store_block(h, hash_block_map[h])#hash, correponding block content  ## use tuple
+
+        if (self.findServerOption == 2):
+            #perform pings, store all blocks to
+            minTime = 999999
+            block2insertIdx = 0
+            for i in range(self.block_num):
+#                pingTime = timeit.timeit('self.block_list[i].ping()', number = 3)
+                pingTime = 0
+                for test in range(3):
+                    startT = time.time()
+                    self.block_list[i].ping()
+                    endT = time.time()
+                    pingTime += endT-startT
+                pingTime /= 3
+                    
+                    
+                if (pingTime < minTime):
+                    block2insertIdx = i
+                    minTime = pingTime
+            block2insert = self.block_list[block2insertIdx]
+            for h in hash_block_map.keys():
+                block2insert.store_block(h, hash_block_map[h])
+
+
+
+
+
+
+
+
+#        print("uploading %s "% filename)
+#        print(hash_block_map)
+#
+
+        #create new file
+        file_version, file_hashlist = self.metadataStore.read_file(filename)#create a new empty one
+
+        #add hashlist for it
+        self.metadataStore.modify_file(filename, file_version + 1, list(hash_block_map.keys()))#use tuple
+        print("OK")
+        return
+
+    """
+	delete(filename) : Signals the MetadataStore to delete a file.
+	"""
+    def delete(self, filename):
+        #check the existence of file
+#        try:
+#            self.metadataStore.delete_file(filename, 0)
+#        except ErrorResponse as e:
+#            if (e.error_type == 2):
+#                self.metadataStore.delete_file(filename, e.current_version + 1)
+#            else:
+#                raise e
+#
+        version, hashlist = self.metadataStore.read_file(filename)
+        if (version == 0 and len(hashlist) == 0):
+            #no exist this filename
+            print("Not Found")
+            pass
+        else:
+            self.metadataStore.delete_file(filename, version + 1)
+            print("OK")
+
+        return
+        """
+        download(filename, dst) : Downloads a file (f) from SurfStore and saves
+        it to (dst) folder. Ensures not to download unnecessary blocks.
+	"""
+    def download(self, filename, location):
+        global DEBUG
+        #get blocks
+        if DEBUG:
+            print("try to download:", filename)
+        curr_version, curr_hashlist = self.metadataStore.read_file(filename)
+        
+        if (curr_version == 0 and len(curr_hashlist) == 0):
+            print("Not Found")
+            return
+
+        if DEBUG:
+            print(curr_version)
+        curr_blocks = []
+
+#        print("currversion" + str(curr_version) + "curr_hashlist" + str(curr_hashlist))
+        for h in curr_hashlist:
+
+            for b in self.block_list:
+                if b.has_block(h):
+                    curr_blocks.append(b.get_block(h))
+                    break
+
+
+        #what if missing block?
+        if (len(curr_blocks) == 0):
+            e = ErrorResponse("missing blocks of " + filename)
+            e.missing_blocks([])
+            raise e
+
+
+        #merge blocks， write file
+        byteContent = b"".join(curr_blocks)
+        with open(os.path.join(location, filename), "wb") as f:
+            f.write(byteContent)
+        print("OK")
+        return
+    """
+	 Use eprint to print debug messages to stderr
+	 E.g -
+	 self.eprint("This is a debug message")
+	"""
+    def eprint(*args, **kwargs):
+        print(*args, file=sys.stderr, **kwargs)
+    def test1(self):
+        self.metadataStore.read_file("fil1.txt")
+
+if __name__ == '__main__':
+    client = SurfStoreClient(sys.argv[1])
+    # print("OK")#only success should we print ok. or we should print out NO found
+
+    operation = sys.argv[2]
+    if operation == 'upload':
+        client.upload(sys.argv[3])
+    elif operation == 'download':
+        client.download(sys.argv[3], sys.argv[4])
+    elif operation == 'delete':
+        client.delete(sys.argv[3])
+    else:
+        print("Invalid operation")
---
+++ metastore.py
@@ -0,0 +1,195 @@
+import rpyc
+import sys
+import copy
+import os
+'''
+A sample ErrorResponse class. Use this to respond to client requests when the request has any of the following issues -
+1. The file being modified has misspping blocks in the block store.
+2. The file being read/deleted does not exist.
+3. The request for modifying/deleting a file has the wrong file version.
+
+You can use this class as it is or come up with your own implementation.
+'''
+
+DEBUG = False
+class ErrorResponse(Exception):
+	def __init__(self, message):
+		super(ErrorResponse, self).__init__(message)
+		self.error = message
+
+	def missing_blocks(self, hashlist):
+		self.error_type = 1
+		self.missing_blocks = hashlist
+
+	def wrong_version_error(self, version):
+		self.error_type = 2
+		self.current_version = version
+
+	def file_not_found(self):
+		self.error_type = 3
+
+def findServer(h, numBlockStores):
+	return int(h,16) % numBlockStores
+
+'''
+The MetadataStore RPC server class.
+
+The MetadataStore process maintains the mapping of filenames to hashlists. All
+metadata is stored in memory, and no database systems or files will be used to
+maintain the data.
+'''
+class MetadataStore(rpyc.Service):
+    class StoreFile():
+        def __init__(self, filename, hashlist):
+            self.filename = filename
+            self.version = 0
+            self.hashlist = hashlist
+    """
+        Initialize the class using the config file provided and also initialize
+        any datastructures you may need.
+	"""
+    def __init__(self, config):
+        super(MetadataStore, self).__init__()
+        self.readConfig(config)
+        self.file_list = {}#filename: storeFileClass
+    def readConfig(self, config):
+        with open(config, "r") as f:
+            lines = f.read().split("\n")
+        self.block_num = int(lines[0].split(":")[-1])
+        self.port = int(lines[1].split(":")[-1])
+        self.block_list = []
+        for i in range(2, 2 + self.block_num):
+            infos = lines[i].split(":")
+            self.block_list.append(rpyc.connect(infos[1].strip(), int(infos[2])).root)
+
+    '''
+        ModifyFile(f,v,hl): Modifies file f so that it now contains the
+        contents refered to by the hashlist hl.  The version provided, v, must
+        be exactly one larger than the current version that the MetadataStore
+        maintains.
+
+        As per rpyc syntax, adding the prefix 'exposed_' will expose this
+        method as an RPC call
+	'''
+    def exposed_modify_file(self, filename, version, hashlist):
+        filename = copy.deepcopy(os.path.basename(filename))
+        version = copy.deepcopy(version)
+        #deep copy hash list
+        new_hashlist = [""]*len(hashlist)
+        for i in range(len(hashlist)):
+            new_hashlist[i] = copy.deepcopy(hashlist[i])
+        global DEBUG
+        if (DEBUG):
+            print("modify", filename,version)
+        #check files
+#        currFile = self.file_list.get(filename, None)
+#        if (currFile == None):
+#            e = ErrorResponse("file not found : " + filename)
+#            e.file_not_found()
+#            raise e
+        currFile = self.file_list.get(filename, None)
+        if (currFile == None):
+            currFile = self.StoreFile(filename, [])
+
+        #check version
+        currVersion = currFile.version
+        if (currVersion != version - 1):
+            e = ErrorResponse("wrong version number of %s, current version is %d, but given %d" % (filename, currVersion, version))
+            e.wrong_version_error(0)#set as 0
+            raise e
+        #update version
+        currFile.version = version
+
+        #check blocks
+        for h in new_hashlist:
+            idxBlockStore = findServer(h, self.block_num)
+            if not self.block_list[idxBlockStore].has_block(h):
+                e = ErrorResponse("missing blocks of " + filename)
+                e.missing_blocks(hashlist)
+                raise e
+
+
+
+        #modify file
+        if DEBUG:
+            print("successfully modify", version, hashlist )
+
+
+        currFile.hashlist = new_hashlist
+        self.file_list[filename] = currFile
+
+
+
+        '''
+        DeleteFile(f,v): Deletes file f. Like ModifyFile(), the provided
+        version number v must be one bigger than the most up-date-date version.
+
+        As per rpyc syntax, adding the prefix 'exposed_' will expose this
+        method as an RPC call
+	'''
+    def exposed_delete_file(self, filename, version):
+        global DEBUG
+        if DEBUG:
+            print("delete", filename, version)
+        filename = copy.deepcopy(os.path.basename(filename))
+        version = copy.deepcopy(version)
+        #check files
+        currFile = self.file_list.get(filename, None)
+        if (currFile == None):#if deleted
+            e = ErrorResponse("file not found" + filename)
+            e.file_not_found()
+            raise e
+
+        if (len(currFile.hashlist) == 0):
+            return
+
+        #check version
+
+        if (currFile.version != version - 1):
+
+            e = ErrorResponse("wrong version number of %s, current version is %d, but given %d" % (filename, currFile.version, version))
+            e.wrong_version_error(currFile.version)
+            raise e
+
+        #update version
+        currFile.version = version
+        #delete file:clean hashlist
+        currFile.hashlist= []
+        return
+    '''
+        (v,hl) = ReadFile(f): Reads the file with filename f, returning the
+        most up-to-date version number v, and the corresponding hashlist hl. If
+        the file does not exist, v will be 0.
+
+        As per rpyc syntax, adding the prefix 'exposed_' will expose this
+        method as an RPC call
+    '''
+    def exposed_read_file(self, filename):
+        global DEBUG
+        if (DEBUG):
+            print("read", filename)
+        filename = copy.deepcopy(os.path.basename(filename))
+
+        currFile = self.file_list.get(filename, None)
+        if (currFile == None):
+
+#            currFile = self.StoreFile(filename, [])
+#            self.file_list[filename] = currFile
+            if DEBUG:
+                print("try to read :", filename, "but not exist")
+            return 0,[]
+        else:
+            if DEBUG:
+                print("try to read :", filename, currFile.version, currFile.hashlist)
+
+            # don't know weather I should do it
+        return currFile.version, currFile.hashlist
+
+
+if __name__ == '__main__':
+    from rpyc.utils.server import ThreadPoolServer
+    port = 6000
+    server = ThreadPoolServer(MetadataStore(sys.argv[1]), port = port)
+    print("OK")
+#    print("start metadatastore in port: " + str(port))
+    server.start()
